fba6cddd851b3eb63c97ae90e66b6da69a1d12fb
ARTHUR ANGELUS ANDRADE DE ALMEIDA <25132783@CFP127ALUNO.LOCAL>
2025-02-26 11:24:51 -0300

    Métodos get e set iniciados com o cálculo do IMC

--- src/br/senai/sp/jandira/clinica/Clinica.java
+++ src/br/senai/sp/jandira/clinica/Clinica.java
@@ -1,3 +1,8 @@
+/*
+ * PROGRAMADOR: Arthur
+ * DATA: 26/02/25
+ * EMPRESA: SENAI
+ */
 package br.senai.sp.jandira.clinica;
 
 import java.time.LocalDate;
@@ -7,20 +12,18 @@ import br.senai.sp.jandira.clinica.model.Paciente;
 public class Clinica {
 
 	public static void main(String[] args) {
-
-		System.out.println("Hello World");
 		
 		// Criando um objeto paciente
 		Paciente p1 = new Paciente();
-		p1.nome = "Ana Maria";
-		p1.altura = 1.65;
+		p1.setNome("Ana Maria");	
+		p1.setAltura(1.65);
 		p1.peso = 51;
 		p1.telefone = "(11)99999-8888";
 		p1.dataNascimento = LocalDate.of(2003, 7, 15);
 		
 		Paciente p2 = new Paciente();
-		p2.nome = "Arthur Angelus";
-		p2.altura = 1.85;
+		p2.setNome("Arthur Angelus");
+		p2.setAltura(1.85);
 		p2.peso = 93;
 		p2.telefone = "(11)64856-8457";
 		p2.dataNascimento = LocalDate.of(2006, 11, 23);
